84b06167bfac465bba018e831a59d52cdaee97c0
vonflafa <119546862+vonflafa@users.noreply.github.com>
2023-05-18 16:12:09 +0200

    Edit playRound() in rockpaperscissors.js Add game() in rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -20,35 +20,66 @@ function getComputerChoice () {
 
   function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
-    const TIE = "It's a TIE!"
+    /*const TIE = "It's a TIE!"
     const WIN =  "You WIN! " + playerSelection + " beats " + computerSelection + ".";
     const LOSE = "You LOSE! " + computerSelection + " beats " + playerSelection + ".";
-    const ERROR = "Something went wrong!";
+    const ERROR = "Something went wrong!";*/
+
     if (playerSelection === "paper") {
       if (computerSelection === "Paper") {
-        return TIE;
+        return 0;
       } else if (computerSelection === "Scissors"){
-        return LOSE;
+        return 1;
       }else {
-        return WIN;
+        return 2;
       }
     } else if (playerSelection === "rock") {
       if(computerSelection === "Rock") {
-        return TIE;
+        return 0;
       } else if (computerSelection === "Paper") {
-        return LOSE;
+        return 1;
       } else {
-        return WIN;
+        return 2;
       }
     } else if (playerSelection === "scissors") {
       if(computerSelection === "Scissors") {
-        return TIE;
+        return 0;
       } else if (computerSelection === "Rock") {
-        return LOSE;
+        return 1;
       } else {
-        return WIN;
+        return 2;
       }
     } else {
-      return ERROR;
+      return NaN;
     }
   }
+
+  function game () {
+    let result = 0;
+    let playerCounter = 0;
+    let computerCounter = 0;
+    for (let i = 0; i = 4; i++) {
+        result = playRound (let playerChoice = prompt("Paper, rock or scissors?"), let computerChoice = getComputerChoice());
+        if (result === 0) {
+            console.log ("This is a TIE!");
+        } else if (result === 1) {
+            console.log("Player WINS! " + playerChoice + " beats " + computerChoice);
+            playerCounter++;
+        }else if (result === 2) {
+            console.log("Computer WINS! " + computerChoice + " beats " + playerChoice);
+            computerCounter++;
+        }
+    }
+    if (playerCounter > computerCounter) {
+        console.log("Player wins with a total score of " + playerCounter + "!");
+    } else if (playerCounter < computerCounter) {
+        console.log("Computer wins with a total score of " + computerCounter + "!");
+    } else {
+        console.log("Something went wrong");
+    }
+  }
+
+  /* gioca un round
+  se vince player dagli un punto
+  se vince computer dagli un punto
+  
